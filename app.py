c0212f6d97f4ebbc667b0bf50625f3b3e0780b0a
ehtishamalik <malikehtishamali334@hotmail.com>
2023-07-26 23:18:14 +0500

    optionally added cors

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+# from flask_cors import CORS
 
 app = Flask(__name__)
+# cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route("/members")
 def members():
