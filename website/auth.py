8e14de8ae2e52047f6106219ff80b7e95a27de97
ehtishamalik <malikehtishamali334@hotmail.com>
2024-07-08 17:05:38 +0500

    Seperated auth file and refactored code

---
+++ website/auth.py
@@ -0,0 +1,81 @@
+from flask import (
+    Blueprint,
+    render_template,
+    redirect,
+    url_for,
+    flash,
+    request,
+    session,
+    current_app,
+)
+from flask_login import login_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import database, User
+from .constants import SUCCESS, ERROR
+
+auth = Blueprint("auth", __name__)
+
+
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        try:
+            user = database.query(User).filter_by(email=email).first()
+            if user and check_password_hash(user.password, password):
+                login_user(user, remember=True)
+                next_page = session.get("next", None)
+                if next_page:
+                    session.pop("next")
+                    return redirect(next_page)
+                return redirect(url_for("views.home"))
+            else:
+                flash("Incorrect Email or Password", category=ERROR)
+        except Exception as e:
+            flash("We could not log you in, please try again", category=ERROR)
+            current_app.logger.error(f"[ERROR]\n{e}")
+
+    if request.args.get("next"):
+        session["next"] = request.args.get("next")
+    return render_template("login.html")
+
+
+@auth.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("auth.login"))
+
+
+@auth.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        if len(username) > 40:
+            flash("Username must be smaller than 40 characters.", category=ERROR)
+        elif len(username) < 8:
+            flash("Username must be longer than 6 characters.", category=ERROR)
+        elif len(password) < 8:
+            flash("Username must be longer than 6 characters.", category=ERROR)
+        else:
+            new_user = User(username, email, generate_password_hash(password))
+            try:
+                database.add(new_user)
+                database.commit()
+            except Exception as e:
+                database.rollback()  # Rollback the session in case of error
+                if hasattr(e, "orig") and "UNIQUE constraint failed: user.email" in str(
+                    e.orig
+                ):
+                    flash("Email already exists.", category=ERROR)
+                else:
+                    flash("Could not register, please try again.", category=ERROR)
+                current_app.logger.error(f"[ERROR]\n{e}")
+            else:
+                flash("Account created successfully, please log in.", category=SUCCESS)
+                return redirect(url_for("auth.login"))
+    return render_template("register.html")
