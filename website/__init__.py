849a8e208002b53b011dc098b28f88d08138a772
ehtishamalik <malikehtishamali334@hotmail.com>
2024-07-09 16:08:26 +0500

    updated HTML and CSS to reduce css

--- website/__init__.py
+++ website/__init__.py
@@ -10,12 +10,16 @@ def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = os.urandom(24).hex()
 
-    app.register_blueprint(views, url_prefix="/")
-    app.register_blueprint(auth, url_prefix="/")
-
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
     login_manager.init_app(app)
+
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+
+    @app.template_filter('include')
+    def include(view):
+        return view in ["home", "about", "contact"]
 
     @login_manager.user_loader
     def load_user(id):
