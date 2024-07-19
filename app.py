fc4eaa63d9bbb4d044474d3d352aeb28925a4481
ehtishamalik <malikehtishamali334@hotmail.com>
2024-07-19 16:38:34 +0500

    Moved logger to the init.py

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
-import logging
-from logging.handlers import RotatingFileHandler
 from website import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    file_handler = RotatingFileHandler("error.log", maxBytes=10240, backupCount=10)
-    file_handler.setLevel(logging.ERROR)
-    formatter = logging.Formatter(
-        "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
-    )
-    file_handler.setFormatter(formatter)
-    app.logger.addHandler(file_handler)
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from logging.handlers import RotatingFileHandler
 from flask_login import LoginManager
+from flask import Flask
+import logging
 import os
 from .views import views
 from .auth import auth
@@ -11,10 +13,21 @@ def create_app():
     app.config["SECRET_KEY"] = os.urandom(24).hex()
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database/database.db"
 
+    # Log Errors
+    file_handler = RotatingFileHandler("error.log", maxBytes=10240, backupCount=10)
+    file_handler.setLevel(logging.ERROR)
+    formatter = logging.Formatter(
+        "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+    )
+    file_handler.setFormatter(formatter)
+    app.logger.addHandler(file_handler)
+
+    # Login functionalities
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
     login_manager.init_app(app)
 
+    # Register Blueprints
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
