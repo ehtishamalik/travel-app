77d69456c4753bc0c26fed33e28914837331f20e
ehtishamalik <malikehtishamali334@hotmail.com>
2024-07-22 23:35:41 +0500

    Updated migrate file

--- migrate_data.py
+++ migrate_data.py
@@ -1,42 +1,45 @@
 from sqlalchemy import create_engine, exc
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from werkzeug.security import generate_password_hash
+from sqlalchemy.orm import sessionmaker, declarative_base
 from src.models import User
 
 # Database connection URI
 SQLALCHEMY_DATABASE_URI = "sqlite:///instance/database.db"  # Adjust as needed
 
 # SQLAlchemy setup
-engine = create_engine(SQLALCHEMY_DATABASE_URI)
+engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
 
 
 # Function to add admin user
-def add_admin_user():
-    admin_user = User(
-        username="admin",
-        email="admin@tt.com",
-        password=generate_password_hash("Eht!sham"),
-        is_admin=True,
-    )
-
-    session.add(admin_user)
-    session.commit()
-    print("Admin user added successfully.")
+admin_user = User(
+    username="admin",
+    email="admin@tt.com",
+    password="Eht!sham",
+    is_admin=True,
+)
 
 
 if __name__ == "__main__":
     try:
         # Create tables if they do not exist
         Base.metadata.create_all(bind=engine)
-    except exc.OperationalError:
-        print("cannot")
-    else:
-        # Add admin user
-        add_admin_user()
 
+        # Add admin user
+        session.add(admin_user)
+        session.commit()
+    except exc.OperationalError:
+        session.rollback()
+        print("Could not make a connection with database.")
+    except exc.IntegrityError as error:
+        session.rollback()
+        print(f"**UNIQUE constraint failed**\n{error}")
+    except Exception as error:
+        session.rollback()
+        print(f"**Something wend wrong**\n{error}")
+    else:
+        print("Admin user added successfully.")
+    finally:
         # Close session
         session.close()
